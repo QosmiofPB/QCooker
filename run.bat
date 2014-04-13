c2117b38e36e8caac5aa14e633f541fa5a8c416b
QosmiofPB <donsjecetrtek@gmail.com>
2014-04-13 20:37:39 +0200

    Delete run.bat

--- run.bat
+++
@@ -1,1 +0,0 @@
-java -cp RSBot-5073.jar;./bin/ org.powerbot.Boot -Xss6m -Xmx2048m -XX:MaxPermSize=500m -XX:+CMSClassUnloadingEnabled -XX:+UseCodeCacheFlushing -XX:-UseSplitVerifier -XX:+UseConcMarkSweepGC -debug
